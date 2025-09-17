a0d475f6e68f50296c82c600c18f7d792ac39b02
jsonnek <jtsonnek@students.unwsp.edu>
2025-09-17 14:40:58 -0500

    Wrote a program to define the age of a person based on user input in program_2.py

--- program_2.py
+++ program_2.py
@@ -1,14 +1,25 @@
+# Jonathan Sonnek
+# Sept 17th, 2025
+# Age Classifier
+
 # Write a program that asks the user to enter a person's age.  The program should display a message indicating whether the person is an infant, a child, a teenager, or an adult.  Following are the guidelines:
 
 # If the person is 1 year old or less, it should display "infant" (without quotes).
 # If the person is older than 1 year, but younger than 13 years, it should display "child".
 # If the person is at least 13 years old, but less than 20 years old, it should display "teenager".
-# If the person is at least 20 year old, it should display "adult".
+# If the person is at least 20 years old, it should display "adult".
 
 def categorize_age(age):
     ageCategory = "TBD"
     ######################
-    # WRITE YOUR CODE HERE
+    if age <= 1:
+        print("Infant")
+    elif age > 1 and age < 13:
+        print("Child")
+    elif age >13 and age < 20:
+        print("Teenager")
+    else:
+        print("Adult")
     ######################
 
 
